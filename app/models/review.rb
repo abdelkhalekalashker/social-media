bc91b4b077e3848928d063c512099c407f466021
abdelkhalekalashker <abdoahm80@gmail.com>
2024-05-06 21:55:25 +0300

    add association to models and add average_rate method to review model to calculate post rate after review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,13 @@
 class Review < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  after_create :update_post_average_rate
+
+  def update_post_average_rate
+    post = Post.find(post_id)
+    # this can be created using counter cache
+    reviews_stars = Review.where(post_id: post).pluck(:stars)
+    post.update(average_rate: (reviews_stars.sum / reviews_stars.size).round(2))
+  end
 end
